8ef6918e4028a39a11133c149d99c84a30243d08
Richa <richa.parekh@pavansgroup.in>
2025-06-04 19:10:59 +0530

    Render the Notes and Note Page

---
+++ Tutorial_03/controllers/index.php
@@ -0,0 +1,3 @@
+<?php
+    $heading = "Dashboard";
+    require("views/index.view.php");
---
+++ Tutorial_03/views/404.php
@@ -0,0 +1,11 @@
+<?php require "partials/head.php"; ?>
+<?php require "partials/nav.php"; ?>
+
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-2xl text-center bg-red-100">Page Not Found</h1>
+        <a class="" href="/">Go Back to Home</a>
+    </div>
+</main>
+
+<?php require 'partials/footer.php'; ?>
