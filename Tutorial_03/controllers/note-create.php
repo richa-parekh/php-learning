c60e50157617e422efffff2e3410f4e1ac9f4fd8
Richa <richa.parekh@pavansgroup.in>
2025-06-05 12:38:33 +0530

    Intro to Form Validation

--- Tutorial_03/controllers/note-create.php
+++ Tutorial_03/controllers/note-create.php
@@ -2,11 +2,24 @@
     $heading = "Create Note";
     $config = require "config.php";
     $db = new Database($config['database']);
+    
     if($_SERVER['REQUEST_METHOD'] === "POST"){
-        $db->query("INSERT INTO notes (user_id, note) VALUES (:user, :note)", [
-            'user' => 1,
-            'note' => $_POST["note"]
-        ]);
+        $errors = [];
+
+        if(strlen($_POST['note']) === 0){
+            $errors['note'] = "Note is required.";
+        }
+        if(strlen($_POST['note']) > 250){
+            $errors['note'] = "Note should be 250 character long.";
+        }
+
+        if(empty($errors)){
+            $db->query("INSERT INTO notes (user_id, note) VALUES (:user_id, :note)", [
+                'user_id' => 1,
+                'note' => $_POST["note"]
+            ]);
+        }
+        
     }
     
     require("views/note-create.view.php");
--- Tutorial_03/views/note-create.view.php
+++ Tutorial_03/views/note-create.view.php
@@ -9,8 +9,8 @@
 
     <form method="POST" class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <label for="note" class="block mb-2 text-lg font-medium text-gray-900 text-grey-600">Your note</label>
-        <textarea id="note" name="note" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>
-
+        <textarea id="note" name="note" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"><?= $_POST['note'] ?? '' ?></textarea>
+        <p class="text-red-500 text-sm mt-2"><?= $errors['note'] ?? '' ?></p>
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <button type="button" class="text-sm/6 font-semibold text-gray-900 cursor-pointer">Cancel</button>
             <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer">Save</button>
