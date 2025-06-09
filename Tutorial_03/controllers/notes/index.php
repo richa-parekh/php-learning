d477de6175b2fe852b0757df740666b56870a341
Richa <richa.parekh@pavansgroup.in>
2025-06-09 11:46:51 +0530

    Make Your First Service Container

--- Tutorial_03/controllers/notes/index.php
+++ Tutorial_03/controllers/notes/index.php
@@ -1,13 +1,15 @@
 <?php
-    use Core\Database;
-    $config = require base_path("config.php");
-    $db = new Database($config['database']);
 
-    $currentUserId = 1;
-    $query = "SELECT * FROM notes WHERE user_id = :user";
-    $notes = $db->query($query, ['user' => $currentUserId])->get();
+use Core\App;
+use Core\Database;
 
-    view("notes/index", [
-        'heading' => "Notes",
-        'notes' => $notes
-    ]);
+$db = App::resolve(Database::class);
+
+$currentUserId = 1;
+$query = "SELECT * FROM notes WHERE user_id = :user";
+$notes = $db->query($query, ['user' => $currentUserId])->get();
+
+view("notes/index", [
+    'heading' => "Notes",
+    'notes' => $notes
+]);
--- Tutorial_03/controllers/notes/show.php
+++ Tutorial_03/controllers/notes/show.php
@@ -1,17 +1,18 @@
 <?php
-    use Core\Database;
-    
-    $config = require base_path("config.php");
-    $db = new Database($config['database']);
 
-    $currentUserId = 1;
-    
-    $query = "SELECT * FROM notes WHERE id = :id";
-    $note = $db->query($query,['id' => $_GET['id']])->findOrFail();
-    
-    authorize($note['user_id'] === $currentUserId);
-    
-    view("notes/show", [
-        'heading' => "Note",
-        'note' => $note
-    ]);
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 1;
+
+$query = "SELECT * FROM notes WHERE id = :id";
+$note = $db->query($query, ['id' => $_GET['id']])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+view("notes/show", [
+    'heading' => "Note",
+    'note' => $note
+]);
