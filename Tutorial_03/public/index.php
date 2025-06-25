e46b34056ce7123977b39bc758dad72c55be4673
Richa <richa.parekh@pavansgroup.in>
2025-06-25 12:12:01 +0530

    Composer and Free Autoloading

--- Tutorial_03/public/index.php
+++ Tutorial_03/public/index.php
@@ -6,12 +6,15 @@ use Core\ValidationException;
 session_start();
 const BASE_PATH = __DIR__ . "/../";
 
+require BASE_PATH . "/vendor/autoload.php";
+
 require BASE_PATH . "Core/functions.php";
 
-spl_autoload_register(function ($class) {
+/* spl_autoload_register(function ($class) {
     $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
     require base_path("{$class}.php");
-});
+}); */
+
 require base_path("bootstrap.php");
 
 $router = new Core\Router;
